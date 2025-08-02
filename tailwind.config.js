70985ddcc262ad9eea224ea9ad189475f5f969f7
lgpecin <lgpecin@gmail.com>
2025-08-02 17:28:45 -0300

    Rewrite homepage and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      }
-    }
+      colors: {
+        primary: '#ff4d00',
+        dark: '#1f1f1f',
+        light: '#f5f5f5',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
